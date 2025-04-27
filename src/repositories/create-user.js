9f4e7cd4e1553dbbbc15afae970f926c1d58934d
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-04-27 17:33:34 -0300

    feat: adicionando PostgresCreateUserRepository

---
+++ src/repositories/create-user.js
@@ -0,0 +1,8 @@
+import { prisma } from '../../prisma/prisma.js'
+export class PostgresCreateUserRepository {
+    async execute(createUserParams) {
+        return await prisma.user.create({
+            data: createUserParams,
+        })
+    }
+}
