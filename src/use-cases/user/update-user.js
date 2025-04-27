585d1ca456e035c36ccf038415b7b912294aff03
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-04-27 20:34:28 -0300

    feat: adicionando UpdateUserUseCase

---
+++ src/use-cases/user/update-user.js
@@ -0,0 +1,38 @@
+import bcrypt from 'bcrypt'
+import { EmailAlreadyInUseError } from '../../errors/user.js'
+
+export class UpdateUserUseCase {
+    constructor(getUserByEmailRepository, updateUserRepository) {
+        this.getUserByEmailRepository = getUserByEmailRepository
+        this.updateUserRepository = updateUserRepository
+    }
+    async execute(userId, updateUserParams) {
+        if (updateUserParams.email) {
+            const userWithProvidedEmail =
+                await this.getUserByEmailRepository.execute(
+                    updateUserParams.email,
+                )
+
+            if (userWithProvidedEmail && userWithProvidedEmail.id !== userId) {
+                throw new EmailAlreadyInUseError(updateUserParams.email)
+            }
+        }
+
+        const user = {
+            ...updateUserParams,
+        }
+
+        if (updateUserParams.password) {
+            const hashedPassword = await bcrypt.hash(
+                updateUserParams.password,
+                10,
+            )
+
+            user.password = hashedPassword
+        }
+
+        const updateUser = await this.updateUserRepository.execute(userId, user)
+
+        return updateUser
+    }
+}
