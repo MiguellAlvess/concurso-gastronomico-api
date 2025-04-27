4519517e51bd5405a5651edc539bcbe3676ed729
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-04-27 20:12:00 -0300

    feat: adicionando rota de GetUserById

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 import express from 'express'
 import dotenv from 'dotenv'
-import { makeCreateUserController } from './src/factories/controllers/user.js'
+import {
+    makeCreateUserController,
+    makeGetUserByIdController,
+} from './src/factories/controllers/user.js'
 
 dotenv.config()
 
 const app = express()
 
 app.use(express.json())
+
+app.get('/api/users/:userId', async (req, res) => {
+    const getUserByIdController = makeGetUserByIdController()
+
+    const { statusCode, body } = await getUserByIdController.execute(req)
+
+    res.status(statusCode).send(body)
+})
 
 app.post('/api/users', async (req, res) => {
     const createUserController = makeCreateUserController()
