d29dc5ecd9e44841b9eac16161b2b8b3537185be
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-04-27 20:40:36 -0300

    feat: adicionando UpdateUserController

---
+++ src/controllers/user/update-user.js
@@ -0,0 +1,48 @@
+import { ZodError } from 'zod'
+import {
+    ok,
+    serverError,
+    badRequest,
+    invalidIdResponse,
+    checkIfIdIsValid,
+} from '../../helpers/index.js'
+
+import { EmailAlreadyInUseError } from '../../errors/user.js'
+import { updateUserSchema } from '../../schemas/index.js'
+export class UpdateUserController {
+    constructor(updateUserUseCase) {
+        this.updateUserUseCase = updateUserUseCase
+    }
+
+    async execute(httpRequest) {
+        try {
+            const userId = httpRequest.params.userId
+
+            const isIdValid = checkIfIdIsValid(userId)
+
+            if (!isIdValid) {
+                return invalidIdResponse()
+            }
+
+            const params = httpRequest.body
+
+            await updateUserSchema.parseAsync(params)
+
+            const updatedUser = await this.updateUserUseCase.execute(
+                userId,
+                params,
+            )
+
+            return ok(updatedUser)
+        } catch (error) {
+            if (error instanceof ZodError) {
+                return badRequest({ message: error.errors[0].message })
+            }
+            if (error instanceof EmailAlreadyInUseError) {
+                return badRequest({ message: error.message })
+            }
+            console.log(error)
+            return serverError()
+        }
+    }
+}
