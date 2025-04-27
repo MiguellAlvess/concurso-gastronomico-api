fed8e980f911cdb52f0b6b972950395aed6e3601
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-04-27 20:47:46 -0300

    feat: adicionando PostgresDeleteUserRepository

---
+++ src/repositories/user/delete-user.js
@@ -0,0 +1,15 @@
+import { prisma } from '../../../prisma/prisma.js'
+
+export class PostgresDeleteUserRepository {
+    async execute(userId) {
+        try {
+            return await prisma.user.delete({
+                where: {
+                    id: userId,
+                },
+            })
+        } catch (error) {
+            return null
+        }
+    }
+}
