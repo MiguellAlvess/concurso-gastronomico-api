3788e0b4d001008eda1a142c0e37281452f66794
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-03 18:34:16 -0300

    feat: adicionando o UpdateRestaurantController

---
+++ src/controllers/restaurant/update-restaurant.js
@@ -0,0 +1,46 @@
+import {
+    serverError,
+    badRequest,
+    checkIfIdIsValid,
+    invalidIdResponse,
+    ok,
+} from '../helpers/index.js'
+import { ZodError } from 'zod'
+import { CnpjAlreadyInUseError } from '../../errors/restaurant.js'
+import { updateRestaurantSchema } from '../../schemas/restaurant.js'
+
+export class UpdateRestaurantController {
+    constructor(updateRestaurantUseCase) {
+        this.updateRestaurantUseCase = updateRestaurantUseCase
+    }
+
+    async execute(httpRequest) {
+        try {
+            const restaurantId = httpRequest.params.restaurantId
+
+            const idIsValid = checkIfIdIsValid(restaurantId)
+
+            if (!idIsValid) {
+                return invalidIdResponse()
+            }
+
+            const params = httpRequest.body
+
+            await updateRestaurantSchema.parseAsync(params)
+
+            const updatedRestaurant =
+                await this.updateRestaurantUseCase.execute(restaurantId, params)
+
+            return ok(updatedRestaurant)
+        } catch (error) {
+            if (error instanceof ZodError) {
+                return badRequest({ message: error.errors[0].message })
+            }
+            if (error instanceof CnpjAlreadyInUseError) {
+                return badRequest({ message: error.message })
+            }
+            console.error(error)
+            return serverError()
+        }
+    }
+}
