038aed60a2520e733cb5668352fabe534561c4ef
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-03 18:25:20 -0300

    feat: adicionando o UpdateRestaurantUseCase

---
+++ src/use-cases/restaurant/update-restaurant.js
@@ -0,0 +1,40 @@
+import bcrypt from 'bcrypt'
+import { CnpjAlreadyInUseError } from '../../errors/restaurant.js'
+
+export class UpdateRestaurantUseCase {
+    constructor(updateRestaurantRepository) {
+        this.updateRestaurantRepository = updateRestaurantRepository
+    }
+
+    async execute(restaurantId, updateRestaurantParams) {
+        if (updateRestaurantParams.cnpj) {
+            const restaurantWithProvidedCnpj =
+                await this.getRestaurantByCnpjRepository.execute(
+                    updateRestaurantParams.cnpj,
+                )
+            if (restaurantWithProvidedCnpj) {
+                throw new CnpjAlreadyInUseError(updateRestaurantParams.cnpj)
+            }
+        }
+
+        const restaurant = {
+            ...updateRestaurantParams,
+        }
+
+        if (updateRestaurantParams.password) {
+            const hashedPassword = await bcrypt.hash(
+                updateRestaurantParams.password,
+                10,
+            )
+
+            restaurant.password = hashedPassword
+        }
+
+        const updateRestaurant = await this.updateRestaurantRepository.execute(
+            restaurantId,
+            restaurant,
+        )
+
+        return updateRestaurant
+    }
+}
