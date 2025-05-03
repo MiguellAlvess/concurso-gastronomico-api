76f0841cd161a695fedb5e9550026cac588417d4
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-03 17:28:42 -0300

    feat: adicionando GetUserByIdController

---
+++ src/controllers/restaurant/get-user-by-id.js
@@ -0,0 +1,36 @@
+import {
+    invalidIdResponse,
+    checkIfIdIsValid,
+    ok,
+    restaurantNotFoundResponse,
+    serverError,
+} from '../helpers/index.js'
+
+export class GetUserByIdController {
+    constructor(getUserByIdUseCase) {
+        this.getUserByIdUseCase = getUserByIdUseCase
+    }
+
+    async execute(htttpRequest) {
+        try {
+            const idIsValid = checkIfIdIsValid(htttpRequest.params.restaurantId)
+
+            if (!idIsValid) {
+                return invalidIdResponse()
+            }
+
+            const restaurant = await this.getUserByIdUseCase.execute(
+                htttpRequest.params.restaurantId,
+            )
+
+            if (!restaurant) {
+                return restaurantNotFoundResponse()
+            }
+
+            return ok(restaurant)
+        } catch (error) {
+            console.error(error)
+            return serverError()
+        }
+    }
+}
