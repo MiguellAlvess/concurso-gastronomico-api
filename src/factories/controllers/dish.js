4da3cc71282eee2978fde9487055b4e64445c8c4
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-05 20:24:57 -0300

    feat: adicionando o makeUpdateDishController

--- src/factories/controllers/dish.js
+++ src/factories/controllers/dish.js
@@ -2,16 +2,19 @@ import {
     PostgresCreateDishRepository,
     PostgresGetRestaurantByIdRepository,
     PostgresGetDishesByRestaurantIdRepository,
+    PostgresUpdateDishRepository,
 } from '../../repositories/index.js'
 
 import {
     CreateDishUseCase,
     GetDishesByRestaurantIdUseCase,
+    UpdateDishUseCase,
 } from '../../use-cases/index.js'
 
 import {
     CreateDishController,
     GetDishesByRestaurantIdController,
+    UpdateDishController,
 } from '../../controllers/index.js'
 
 export const makeCreateDishController = () => {
@@ -39,4 +42,11 @@ export const makeGetDishesByRestaurantIdController = () => {
     const getDishesByRestaurantIdController =
         new GetDishesByRestaurantIdController(getDishesByRestaurantIdUseCase)
     return getDishesByRestaurantIdController
+}
+
+export const makeUpdateDishController = () => {
+    const updateDishRepository = new PostgresUpdateDishRepository()
+    const updateDishUseCase = new UpdateDishUseCase(updateDishRepository)
+    const updateDishController = new UpdateDishController(updateDishUseCase)
+    return updateDishController
 }
