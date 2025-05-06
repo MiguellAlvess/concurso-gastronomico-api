0c6ec8a285f9971b379f93adbf9222e64556f820
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-06 15:55:26 -0300

    feat: garantido que o reviewNotFoundResponse seja chamadao caso não exista avaliação

--- src/controllers/review/delete-review.js
+++ src/controllers/review/delete-review.js
@@ -3,6 +3,7 @@ import {
     ok,
     invalidIdResponse,
     checkIfIdIsValid,
+    reviewNotFoundResponse,
 } from '../helpers/index.js'
 
 export class DeleteReviewController {
@@ -21,6 +22,10 @@ export class DeleteReviewController {
             }
 
             const deletedReview = await this.deleteReviewUseCase.execute(dishId)
+
+            if (!deletedReview) {
+                return reviewNotFoundResponse()
+            }
 
             return ok(deletedReview)
         } catch (error) {
