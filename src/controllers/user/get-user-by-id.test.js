42031f8fb696c5ca547a80c4a5acd305c38e1229
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-07 10:32:31 -0300

    test: garantindo que o GetUserByIdController retorne 200 quando o usuário for encontrado

---
+++ src/controllers/user/get-user-by-id.test.js
@@ -0,0 +1,32 @@
+import { user } from '../../tests/index.js'
+import { GetUserByIdController } from './get-user-by-id'
+
+describe('Get User By Id Controller', () => {
+    class GetUserByIdUseCaseStub {
+        async execute() {
+            return user
+        }
+    }
+
+    const makeSut = () => {
+        const getUserByIdUseCase = new GetUserByIdUseCaseStub()
+        const sut = new GetUserByIdController(getUserByIdUseCase)
+
+        return { sut, getUserByIdUseCase }
+    }
+
+    it('should return 200 when user is found', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        // act
+        const response = await sut.execute({
+            params: {
+                userId: user.id,
+            },
+        })
+
+        // assert
+        expect(response.statusCode).toBe(200)
+    })
+})
