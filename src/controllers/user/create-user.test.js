a8d1e1b6df26ee77a9e4da9ae1411ef45f4cd5a5
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-07 09:30:02 -0300

    test: garantindo que o CreateUserController retorne 400 o first_name não for fornecido

--- src/controllers/user/create-user.test.js
+++ src/controllers/user/create-user.test.js
@@ -32,4 +32,19 @@ describe('Create User Controller', () => {
         expect(response.statusCode).toBe(201)
         expect(response.body).toEqual(user)
     })
+    it('should return 400 if first_name is not provided', async () => {
+        // arrange
+        const { sut } = makeSut()
+
+        // act
+        const result = await sut.execute({
+            body: {
+                ...httpRequest,
+                first_name: undefined,
+            },
+        })
+
+        // assert
+        expect(result.statusCode).toBe(400)
+    })
 })
