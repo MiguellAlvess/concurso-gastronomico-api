cde058da3dfc8fcca2a67873b7fea4d3fa18189d
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-08 09:38:43 -0300

    feat: garantindo que o DeleteRestaurantRepository lançe uma execeção se o prisma lançar uma exceção

--- src/repositories/restaurant/delete-restaurant.js
+++ src/repositories/restaurant/delete-restaurant.js
@@ -1,4 +1,6 @@
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/library'
 import { prisma } from '../../../prisma/prisma.js'
+import { RestaurantNotFoundErrort } from '../../errors/restaurant.js'
 
 export class PostgresDeleteRestaurantRepository {
     async execute(restaurantId) {
@@ -9,7 +11,14 @@ export class PostgresDeleteRestaurantRepository {
                 },
             })
         } catch (error) {
-            return null
+            if (error instanceof PrismaClientKnownRequestError) {
+                // P2025 -> An operation failed because it depends on one or more records that were required but not found. {cause}
+                if (error.code === 'P2025') {
+                    throw new RestaurantNotFoundErrort(restaurantId)
+                }
+            }
+
+            throw error
         }
     }
 }
