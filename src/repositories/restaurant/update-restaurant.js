9a52db96bc57050cac8381be65bbaf977e592499
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-12 07:41:31 -0300

    feat: garantindo que o UpdateRestaurantRepository lance uma exceção se o Prisma lançar um erro

--- src/repositories/restaurant/update-restaurant.js
+++ src/repositories/restaurant/update-restaurant.js
@@ -1,12 +1,24 @@
 import { prisma } from '../../../prisma/prisma.js'
+import { RestaurantNotFoundError } from '../../errors/restaurant.js'
 
 export class PostgresUpdateRestaurantRepository {
     async execute(restaurantId, updateRestaurantParams) {
-        return await prisma.restaurant.update({
-            where: {
-                id: restaurantId,
-            },
-            data: updateRestaurantParams,
-        })
+        try {
+            return await prisma.restaurant.update({
+                where: {
+                    id: restaurantId,
+                },
+                data: updateRestaurantParams,
+            })
+        } catch (error) {
+            if (error.name === 'PrismaClientKnownRequestError') {
+                // P2025 -> An operation failed because it depends on one or more records that were required but not found. {cause}
+                if (error.code === 'P2025') {
+                    throw new RestaurantNotFoundError(restaurantId)
+                }
+            }
+
+            throw error
+        }
     }
 }
