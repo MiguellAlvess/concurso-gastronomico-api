2bf96549ca4dcb77571386982cd85662bb1e5e07
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-12 07:44:29 -0300

    feat: garantindo que o UpdateDishRepository lance uma exceção se o Prisma lançar um erro

--- src/repositories/dish/update-dish.js
+++ src/repositories/dish/update-dish.js
@@ -1,12 +1,24 @@
 import { prisma } from '../../../prisma/prisma.js'
+import { DishNotFoundError } from '../../errors/dish.js'
 
 export class PostgresUpdateDishRepository {
     async execute(dishId, updateDishParams) {
-        return await prisma.dish.update({
-            where: {
-                id: dishId,
-            },
-            data: updateDishParams,
-        })
+        try {
+            return await prisma.dish.update({
+                where: {
+                    id: dishId,
+                },
+                data: updateDishParams,
+            })
+        } catch (error) {
+            if (error.name === 'PrismaClientKnownRequestError') {
+                // P2025 -> An operation failed because it depends on one or more records that were required but not found. {cause}
+                if (error.code === 'P2025') {
+                    throw new DishNotFoundError(dishId)
+                }
+            }
+
+            throw error
+        }
     }
 }
