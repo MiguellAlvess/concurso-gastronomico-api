af15473ac07fb288c39f4dfc02d2c8096ea4d79f
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-17 17:57:29 -0300

    feat: garantindo que o middleware de autenticação seja utilizado nas rotas de avaliação

--- src/routes/reviews.js
+++ src/routes/reviews.js
@@ -5,10 +5,11 @@ import {
     makeGetReviewsByDishIdController,
     makeDeleteReviewController,
 } from '../factories/controllers/review.js'
+import { auth } from '../middlewares/auth.js'
 
 export const reviewsRouter = Router()
 
-reviewsRouter.get('/by-user', async (req, res) => {
+reviewsRouter.get('/by-user', auth, async (req, res) => {
     const getReviewsByUserIdController = makeGetReviewsByUserIdController()
 
     const { statusCode, body } = await getReviewsByUserIdController.execute(req)
@@ -16,7 +17,7 @@ reviewsRouter.get('/by-user', async (req, res) => {
     res.status(statusCode).send(body)
 })
 
-reviewsRouter.get('/by-dish', async (req, res) => {
+reviewsRouter.get('/by-dish', auth, async (req, res) => {
     const getReviewsByDishIdController = makeGetReviewsByDishIdController()
 
     const { statusCode, body } = await getReviewsByDishIdController.execute(req)
@@ -24,7 +25,7 @@ reviewsRouter.get('/by-dish', async (req, res) => {
     res.status(statusCode).send(body)
 })
 
-reviewsRouter.post('/', async (req, res) => {
+reviewsRouter.post('/', auth, async (req, res) => {
     const createReviewController = makeCreateReviewController()
 
     const { statusCode, body } = await createReviewController.execute(req)
@@ -32,7 +33,7 @@ reviewsRouter.post('/', async (req, res) => {
     res.status(statusCode).json(body)
 })
 
-reviewsRouter.delete('/:reviewId', async (req, res) => {
+reviewsRouter.delete('/:reviewId', auth, async (req, res) => {
     const deleteReviewController = makeDeleteReviewController()
 
     const { statusCode, body } = await deleteReviewController.execute(req)
