c57a830999f8c769cf922bfbb50e3969479700a8
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-19 08:10:42 -0300

    feat: adicionando o loginUserSchema

--- src/schemas/user.js
+++ src/schemas/user.js
@@ -41,3 +41,25 @@ export const createUserSchema = z.object({
 export const updateUserSchema = createUserSchema.partial().strict({
     message: 'Some provided field is not allowed',
 })
+
+export const loginUserSchema = z.object({
+    email: z
+        .string({
+            required_error: 'Email is required',
+        })
+        .trim()
+        .email({
+            message: 'Please provide a valid email address',
+        })
+        .min(1, {
+            message: 'Email is required',
+        }),
+    password: z
+        .string({
+            required_error: 'Password is required',
+        })
+        .trim()
+        .min(6, {
+            message: 'Password must be at least 6 characters',
+        }),
+})
