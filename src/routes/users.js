d2c4fd485fbf123af3e8880a442c1d0d223221d5
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-21 17:02:13 -0300

    feat: garantindo que apenas dados de usuários autenticados possam ser obtidos

--- src/routes/users.js
+++ src/routes/users.js
@@ -10,10 +10,13 @@ import { auth } from '../middlewares/auth.js'
 
 export const usersRouter = Router()
 
-usersRouter.get('/:userId', auth, async (req, res) => {
+usersRouter.get('/', auth, async (req, res) => {
     const getUserByIdController = makeGetUserByIdController()
 
-    const { statusCode, body } = await getUserByIdController.execute(req)
+    const { statusCode, body } = await getUserByIdController.execute({
+        ...req,
+        params: { userId: req.userId },
+    })
 
     res.status(statusCode).send(body)
 })
@@ -26,18 +29,24 @@ usersRouter.post('/', async (req, res) => {
     res.status(statusCode).json(body)
 })
 
-usersRouter.patch('/:userId', auth, async (req, res) => {
+usersRouter.patch('/', auth, async (req, res) => {
     const updateUserController = makeUpdateUserController()
 
-    const { statusCode, body } = await updateUserController.execute(req)
+    const { statusCode, body } = await updateUserController.execute({
+        ...req,
+        params: { userId: req.userId },
+    })
 
     res.status(statusCode).json(body)
 })
 
-usersRouter.delete('/:userId', auth, async (req, res) => {
+usersRouter.delete('/', auth, async (req, res) => {
     const deleteUserController = makeDeleteUserController()
 
-    const { statusCode, body } = await deleteUserController.execute(req)
+    const { statusCode, body } = await deleteUserController.execute({
+        ...req,
+        params: { userId: req.userId },
+    })
 
     res.status(statusCode).json(body)
 })
