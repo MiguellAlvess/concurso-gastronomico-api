203df0603a655735cb5892da2d4ac368420cabcf
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-23 19:15:21 -0300

    feat: garantindo que o PostgresGetDishReviewsRepository retorne a média das avaliações do prato

--- src/repositories/dish/get-dish-reviews.js
+++ src/repositories/dish/get-dish-reviews.js
@@ -2,10 +2,26 @@ import { prisma } from '../../../prisma/prisma.js'
 
 export class PostgresGetDishReviewsRepository {
     async execute(dishId) {
-        return await prisma.review.findMany({
+        const reviews = await prisma.review.findMany({
             where: {
                 dish_id: dishId,
             },
         })
+
+        const { _avg: average } = await prisma.review.aggregate({
+            where: {
+                dish_id: dishId,
+            },
+            _avg: {
+                rating: true,
+            },
+        })
+
+        return {
+            reviews,
+            averageRating: average.rating
+                ? Number(average.rating.toFixed(1))
+                : 0,
+        }
     }
 }
