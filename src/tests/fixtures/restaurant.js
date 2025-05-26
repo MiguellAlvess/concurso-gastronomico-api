906b98c059ce891fc08ee759bfb9332bd6f31ebe
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-26 17:50:08 -0300

    feat: adicionando dados de arquivo à fixture de restaurante

--- src/tests/fixtures/restaurant.js
+++ src/tests/fixtures/restaurant.js
@@ -2,9 +2,12 @@ import { faker } from '@faker-js/faker'
 
 export const restaurant = {
     id: faker.string.uuid(),
-    name: faker.person.firstName(),
+    name: faker.company.name(),
     cnpj: '40.947.067/0001-52',
-    password: faker.internet.password({
-        length: 6,
-    }),
+    password: faker.internet.password({ length: 6 }),
+    image_url: 'imagetest.png',
+    file: {
+        filename: 'imagetest.png',
+        mimetype: 'image/png',
+    },
 }
