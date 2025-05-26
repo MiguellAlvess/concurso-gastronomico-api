08f6d0b800e18c3fd5ddf5090c4572cb21630648
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-26 18:54:17 -0300

    test: garantindo que o CreateDishController retorne 400 quando o preço não fornecido

--- src/controllers/dish/create-dish.test.js
+++ src/controllers/dish/create-dish.test.js
@@ -50,4 +50,17 @@ describe('Create Dish Controller', () => {
 
         expect(result.statusCode).toBe(400)
     })
+
+    it('should return 400 when price is not provided', async () => {
+        const { sut } = makeSut()
+
+        const result = await sut.execute({
+            body: {
+                ...httpRequest,
+                price: undefined,
+            },
+        })
+
+        expect(result.statusCode).toBe(400)
+    })
 })
