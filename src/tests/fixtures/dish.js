1d79ee5b90d79f460fa2161a4592696e80b88c1b
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-26 18:48:38 -0300

    fix: ajustando preço para string na fixture de prato

--- src/tests/fixtures/dish.js
+++ src/tests/fixtures/dish.js
@@ -4,7 +4,7 @@ export const dish = {
     id: faker.string.uuid(),
     name: faker.commerce.productName(),
     details: faker.commerce.productDescription().slice(0, 255),
-    price: Number(faker.finance.amount()),
+    price: faker.finance.amount(),
     restaurant_id: faker.string.uuid(),
     image_url: 'imagetest.png',
     file: {
