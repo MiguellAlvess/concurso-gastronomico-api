b95ccd97002d39330b71896ac7f443821dec9fd0
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-27 18:11:41 -0300

    test: garantindo que o GetUserReviewsControler rtorne 200 quand pegar todas as avaliações do usuário

---
+++ src/controllers/user/get-user-reviews.test.js
@@ -0,0 +1,30 @@
+import { GetUserReviewsController } from './get-user-reviews'
+import { review } from '../../tests/index'
+import { faker } from '@faker-js/faker'
+
+describe('Get User Reviews Controller', () => {
+    class GetUserReviewsUseCaseStub {
+        async execute() {
+            return review
+        }
+    }
+
+    const makeSut = () => {
+        const getUserReviewsUseCase = new GetUserReviewsUseCaseStub()
+        const sut = new GetUserReviewsController(getUserReviewsUseCase)
+
+        return { sut, getUserReviewsUseCase }
+    }
+
+    it('should return 200 when getting user reviews successfully', async () => {
+        const { sut } = makeSut()
+
+        const response = await sut.execute({
+            query: {
+                userId: faker.string.uuid(),
+            },
+        })
+
+        expect(response.statusCode).toBe(200)
+    })
+})
