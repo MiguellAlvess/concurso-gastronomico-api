345a0c10c76f41accdc9e31bba6c70f5f92b0db4
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-27 09:50:39 -0300

    test: garantindo que o DeleteReviewControler retorne 200 quando uma avaliação for deletada com êxito

---
+++ src/controllers/review/delete-review.test.js
@@ -0,0 +1,30 @@
+import { DeleteReviewController } from './delete-review.js'
+import { review } from '../../tests/index.js'
+import { faker } from '@faker-js/faker'
+
+describe('Delete Review Controller', () => {
+    class DeleteReviewUseCaseStub {
+        async execute() {
+            return review
+        }
+    }
+
+    const makeSut = () => {
+        const deleteReviewUseCase = new DeleteReviewUseCaseStub()
+        const sut = new DeleteReviewController(deleteReviewUseCase)
+
+        return { sut, deleteReviewUseCase }
+    }
+
+    it('should return 200 when deleting a review successfully', async () => {
+        const { sut } = makeSut()
+
+        const response = await sut.execute({
+            params: {
+                reviewId: faker.string.uuid(),
+            },
+        })
+
+        expect(response.statusCode).toBe(200)
+    })
+})
