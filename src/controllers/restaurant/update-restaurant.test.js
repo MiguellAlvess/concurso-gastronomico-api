74e1dbfcd7de8795dd86cd0b1fe33079a8d1f596
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-27 11:00:17 -0300

    test: garantindo que o UpdateRestaurantController retorne 200 quando o restaurante for atualizado

---
+++ src/controllers/restaurant/update-restaurant.test.js
@@ -0,0 +1,36 @@
+import { UpdateRestaurantController } from './update-restaurant.js'
+import { restaurant } from '../../tests/index.js'
+
+describe('Update Restaurant Controller', () => {
+    class UpdateRestaurantUseCaseStub {
+        async execute() {
+            return restaurant
+        }
+    }
+
+    const makeSut = () => {
+        const updateRestaurantUseCase = new UpdateRestaurantUseCaseStub()
+        const sut = new UpdateRestaurantController(updateRestaurantUseCase)
+
+        return { sut, updateRestaurantUseCase }
+    }
+    const httpRequest = {
+        params: {
+            restaurantId: restaurant.id,
+        },
+        body: {
+            name: restaurant.name,
+            cnpj: restaurant.cnpj,
+            password: restaurant.password,
+        },
+        file: restaurant.file,
+    }
+
+    it('should return 200 when updating a restaurant successfully', async () => {
+        const { sut } = makeSut()
+
+        const response = await sut.execute(httpRequest)
+
+        expect(response.statusCode).toBe(200)
+    })
+})
