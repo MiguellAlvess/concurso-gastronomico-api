abf4cff99de0bae54ee65f95402bad583bdee881
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-26 21:20:21 -0300

    test: garantindo que o UpdateDishController chame o UpdateDishUseCase com os parâmetros corretos

--- src/controllers/dish/update-dish.test.js
+++ src/controllers/dish/update-dish.test.js
@@ -1,6 +1,6 @@
+import { faker } from '@faker-js/faker'
 import { UpdateDishController } from './update-dish.js'
 import { dish } from '../../tests/index.js'
-import { faker } from '@faker-js/faker'
 import { DishNotFoundError } from '../../errors/dish.js'
 
 describe('Update Dish Controller', () => {
@@ -74,5 +74,28 @@ describe('Update Dish Controller', () => {
         const response = await sut.execute(httpRequest)
 
         expect(response.statusCode).toBe(500)
+    })
+
+    it('should call UpdateDishUseCase with correct params', async () => {
+        const { sut, updateDishUseCase } = makeSut()
+        const executeSpy = import.meta.jest.spyOn(updateDishUseCase, 'execute')
+        const dishId = faker.string.uuid()
+
+        await sut.execute({
+            params: { dishId },
+            body: {
+                name: dish.name,
+                image_url: dish.image_url,
+                restaurant_id: dish.restaurant_id,
+            },
+            file: dish.file,
+        })
+
+        expect(executeSpy).toHaveBeenCalledWith(dishId, {
+            name: dish.name,
+            image_url: dish.image_url,
+            restaurant_id: dish.restaurant_id,
+            imageFilename: dish.file.filename,
+        })
     })
 })
