08b919e7ef7c03c7108441540983fb923eeac52c
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-27 17:54:41 -0300

    test: garantindo que o RefreshTokenController retorne 200 se o refresh token fornecido for válido

---
+++ src/controllers/user/refresh-token.test.js
@@ -0,0 +1,29 @@
+import { RefreshTokenUserController } from './refresh-token.js'
+
+describe('Refresh Token User Controller', () => {
+    class RefreshTokenUserUseCaseStub {
+        execute() {
+            return {
+                accessToken: 'any_acess_token',
+                refreshToken: 'any_refresh_token',
+            }
+        }
+    }
+    const makeSut = () => {
+        const refreshTokenUserUseCase = new RefreshTokenUserUseCaseStub()
+        const sut = new RefreshTokenUserController(refreshTokenUserUseCase)
+        return { sut, refreshTokenUserUseCase }
+    }
+
+    it('should return 200 if refresh token is valid', async () => {
+        const { sut } = makeSut()
+
+        const response = await sut.execute({
+            body: {
+                refreshToken: 'any_refresh_token',
+            },
+        })
+
+        expect(response.statusCode).toBe(200)
+    })
+})
