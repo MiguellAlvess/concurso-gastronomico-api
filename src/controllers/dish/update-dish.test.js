7732810592e4bd5e3a307379278e0f0ece0da823
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-26 21:06:24 -0300

    test: garantindo que o UpdateDishController retorne 200 quando o prato for atualizado com sucesso

---
+++ src/controllers/dish/update-dish.test.js
@@ -0,0 +1,38 @@
+import { UpdateDishController } from './update-dish.js'
+import { dish } from '../../tests/index.js'
+import { faker } from '@faker-js/faker'
+
+describe('Update Dish Controller', () => {
+    class UpdateDishUseCaseStub {
+        async execute() {
+            return dish
+        }
+    }
+    const makeSut = () => {
+        const updateDishUseCase = new UpdateDishUseCaseStub()
+        const sut = new UpdateDishController(updateDishUseCase)
+
+        return { sut, updateDishUseCase }
+    }
+
+    const httpRequest = {
+        params: {
+            dishId: faker.string.uuid(),
+        },
+        body: {
+            id: faker.string.uuid(),
+            name: dish.name,
+            image_url: dish.image_url,
+            restaurant_id: dish.restaurant_id,
+        },
+        file: dish.file,
+    }
+
+    it('should return 200 when updating a dish successfully', async () => {
+        const { sut } = makeSut()
+
+        const response = await sut.execute(httpRequest)
+
+        expect(response.statusCode).toBe(200)
+    })
+})
