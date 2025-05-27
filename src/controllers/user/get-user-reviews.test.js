ef129fdabcf88150a36fb8b8f3d63ba470bb0b18
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-27 18:14:49 -0300

    test: garantindo que o GetUserReviewsController retorne 404 se o usuário não for encontrado

--- src/controllers/user/get-user-reviews.test.js
+++ src/controllers/user/get-user-reviews.test.js
@@ -1,6 +1,7 @@
+import { faker } from '@faker-js/faker'
+import { UserNotFoundError } from '../../errors/index.js'
 import { GetUserReviewsController } from './get-user-reviews'
 import { review } from '../../tests/index'
-import { faker } from '@faker-js/faker'
 
 describe('Get User Reviews Controller', () => {
     class GetUserReviewsUseCaseStub {
@@ -38,5 +39,20 @@ describe('Get User Reviews Controller', () => {
         })
 
         expect(response.statusCode).toBe(400)
+    })
+
+    it('should return 404 if user is not found', async () => {
+        const { sut, getUserReviewsUseCase } = makeSut()
+        import.meta.jest
+            .spyOn(getUserReviewsUseCase, 'execute')
+            .mockRejectedValueOnce(new UserNotFoundError())
+
+        const response = await sut.execute({
+            query: {
+                userId: faker.string.uuid(),
+            },
+        })
+
+        expect(response.statusCode).toBe(404)
     })
 })
