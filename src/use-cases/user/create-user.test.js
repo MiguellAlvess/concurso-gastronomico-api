6dba3be5123e6bac88dc3beba7420ee0798cf483
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-28 08:09:40 -0300

    test: garantindo que o CreateUserUseCase crie um usuário corretamente

---
+++ src/use-cases/user/create-user.test.js
@@ -0,0 +1,69 @@
+import { CreateUserUseCase } from './create-user.js'
+import { user } from '../../tests/index.js'
+
+describe('Create User Use Case', () => {
+    class GetUserByEmailRepositoryStub {
+        async execute() {
+            return null
+        }
+    }
+    class CreateUserRepositoryStub {
+        async execute() {
+            return user
+        }
+    }
+
+    class PasswordHasherAdapterStub {
+        async execute() {
+            return 'hashed_password'
+        }
+    }
+
+    class TokensGeneratorAdapterStub {
+        execute() {
+            return {
+                acessToken: 'any_acess_token',
+                refreshToken: 'any_refresh_token',
+            }
+        }
+    }
+
+    class IdGeneratorAdapterStub {
+        execute() {
+            return 'generated_id'
+        }
+    }
+    const makeSut = () => {
+        const getUserByEmailRepository = new GetUserByEmailRepositoryStub()
+        const createUserRepository = new CreateUserRepositoryStub()
+        const passwordHasherAdapter = new PasswordHasherAdapterStub()
+        const idGeneratorAdapter = new IdGeneratorAdapterStub()
+        const tokensGeneratorUserAdapter = new TokensGeneratorAdapterStub()
+        const sut = new CreateUserUseCase(
+            getUserByEmailRepository,
+            createUserRepository,
+            passwordHasherAdapter,
+            idGeneratorAdapter,
+            tokensGeneratorUserAdapter,
+        )
+
+        return {
+            sut,
+            getUserByEmailRepository,
+            createUserRepository,
+            passwordHasherAdapter,
+            idGeneratorAdapter,
+            tokensGeneratorUserAdapter,
+        }
+    }
+
+    it('should successfully create a user', async () => {
+        const { sut } = makeSut()
+
+        const createdUser = await sut.execute(user)
+
+        expect(createdUser).toBeTruthy()
+        expect(createdUser.tokens.acessToken).toBeDefined()
+        expect(createdUser.tokens.refreshToken).toBeDefined()
+    })
+})
