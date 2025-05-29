c759949ef26bd7ea35d1bda500a341356e6d2545
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-29 11:33:10 -0300

    test: garantindo PostgresUpdateUserRepository lança UserNotFoundError se o Prisma lançar P2025 error

--- src/repositories/user/update-user.test.js
+++ src/repositories/user/update-user.test.js
@@ -2,6 +2,7 @@ import { user } from '../../tests/index.js'
 import { PostgresUpdateUserRepository } from './update-user.js'
 import { prisma } from '../../../prisma/prisma.js'
 import { faker } from '@faker-js/faker'
+import { UserNotFoundError } from '../../errors/user.js'
 
 describe('Update User Repository', () => {
     const updateUserParams = {
@@ -48,5 +49,17 @@ describe('Update User Repository', () => {
         const promise = sut.execute(user.id, updateUserParams)
 
         await expect(promise).rejects.toThrow()
+    })
+
+    it('should throw UserNotFoundError if Prisma throws P2025 error', async () => {
+        const sut = new PostgresUpdateUserRepository()
+        import.meta.jest.spyOn(prisma.user, 'update').mockRejectedValueOnce({
+            name: 'PrismaClientKnownRequestError',
+            code: 'P2025',
+        })
+
+        const promise = sut.execute(user.id, updateUserParams)
+
+        await expect(promise).rejects.toThrow(new UserNotFoundError(user.id))
     })
 })
