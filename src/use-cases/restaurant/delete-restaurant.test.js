8429b889d34aa8fcff0b3bb75fa1a2081f5c3757
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-29 09:11:49 -0300

    test: garantindo que o DeleteRestaurantUseCase delete o restaurante com sucesso

---
+++ src/use-cases/restaurant/delete-restaurant.test.js
@@ -0,0 +1,25 @@
+import { faker } from '@faker-js/faker'
+import { DeleteRestaurantUseCase } from './delete-restaurant.js'
+import { restaurant } from '../../tests/index.js'
+
+describe('Delete Restaurant Use Case', () => {
+    class DeleteRestaurantRepositoryStub {
+        async execute() {
+            return restaurant
+        }
+    }
+    const makeSut = () => {
+        const deleteRestaurantRepository = new DeleteRestaurantRepositoryStub()
+        const sut = new DeleteRestaurantUseCase(deleteRestaurantRepository)
+
+        return { sut, deleteRestaurantRepository }
+    }
+
+    it('should successfully delete a restaurant', async () => {
+        const { sut } = makeSut()
+
+        const deletedRestaurant = await sut.execute(faker.string.uuid())
+
+        expect(deletedRestaurant).toEqual(restaurant)
+    })
+})
