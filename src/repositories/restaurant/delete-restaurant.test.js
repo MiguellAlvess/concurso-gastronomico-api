21943d6b16b55c93f18ba105b51d1dcf4650b317
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-29 15:30:53 -0300

    test: garantindo PostgresDeleteRestaurantRepository lançe um erro generico se o Prisma falhar

--- src/repositories/restaurant/delete-restaurant.test.js
+++ src/repositories/restaurant/delete-restaurant.test.js
@@ -6,7 +6,7 @@ describe('Delete Restaurant Repository', () => {
     const createRestaurantParams = {
         id: faker.string.uuid(),
         name: faker.company.name(),
-        cnpj: '40.947.067/0001-52',
+        cnpj: '40.947.067/0002-52',
         password: faker.internet.password({ length: 6 }),
         image_url: 'imagetest.png',
     }
