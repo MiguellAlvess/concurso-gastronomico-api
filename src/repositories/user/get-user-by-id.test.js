0bc4c55ff9cf60f684126e2e442cef5cb893f085
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-29 11:19:25 -0300

    test: garantindo que o PostgresGetUserByIdRepository pegue o usuário por id no banco de dados

---
+++ src/repositories/user/get-user-by-id.test.js
@@ -0,0 +1,19 @@
+import { user } from '../../tests/index.js'
+import { PostgresGetUserByIdRepository } from './get-user-by-id.js'
+import { prisma } from '../../../prisma/prisma.js'
+
+describe('Get User By Id Repository', () => {
+    it('should get user by id on db', async () => {
+        await prisma.user.create({
+            data: user,
+        })
+        const sut = new PostgresGetUserByIdRepository()
+
+        const result = await sut.execute(user.id)
+
+        expect(result.id).toBe(user.id)
+        expect(result.name).toBe(user.name)
+        expect(result.email).toBe(user.email)
+        expect(result.password).toBe(user.password)
+    })
+})
