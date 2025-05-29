81efdadd4a5d9b67c991c915f98cf3df8e2cd009
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-29 15:41:38 -0300

    test: garantindo que o PostgresGetRestaurantByCnpjRepository pegue o restaurante por cnpj no db

---
+++ src/repositories/restaurant/get-restaurant-by-cnpj.test.js
@@ -0,0 +1,28 @@
+import { faker } from '@faker-js/faker'
+import { PostgresGetRestaurantByCnpjRepository } from './get-restaurant-by-cnpj.js'
+import { prisma } from '../../../prisma/prisma.js'
+
+describe('Get Restaurant By Cnpj Repository', () => {
+    const createRestaurantParams = {
+        id: faker.string.uuid(),
+        name: faker.company.name(),
+        cnpj: '40.947.067/0001-52',
+        password: faker.internet.password({ length: 6 }),
+        image_url: 'imagetest.png',
+    }
+
+    it('should get restaurant by cnpj on db', async () => {
+        await prisma.restaurant.create({
+            data: createRestaurantParams,
+        })
+        const sut = new PostgresGetRestaurantByCnpjRepository()
+
+        const result = await sut.execute(createRestaurantParams.cnpj)
+
+        expect(result.id).toBe(createRestaurantParams.id)
+        expect(result.name).toBe(createRestaurantParams.name)
+        expect(result.cnpj).toBe(createRestaurantParams.cnpj)
+        expect(result.password).toBe(createRestaurantParams.password)
+        expect(result.image_url).toBe(createRestaurantParams.image_url)
+    })
+})
