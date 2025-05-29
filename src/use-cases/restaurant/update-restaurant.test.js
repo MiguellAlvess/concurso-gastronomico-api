29c1fa851fa3697a1aa14e2737a72d29609661c1
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-29 10:42:26 -0300

    test: garantindo que o UpdateRestaurantUseCase atualize o restaurante com sucesso (sem cnpj e senha)

---
+++ src/use-cases/restaurant/update-restaurant.test.js
@@ -0,0 +1,51 @@
+import { UpdateRestaurantUseCase } from './update-restaurant.js'
+import { restaurant } from '../../tests/index.js'
+import { faker } from '@faker-js/faker'
+
+describe('Update Restaurant Use Case', () => {
+    class GetRestaurantByCnpjRepositoryStub {
+        async execute() {
+            return null
+        }
+    }
+
+    class UpdateRestaurantRepositoryStub {
+        async execute() {
+            return restaurant
+        }
+    }
+
+    class PasswordHasherAdapterStub {
+        async execute() {
+            return 'hashed_password'
+        }
+    }
+
+    const makeSut = () => {
+        const getRestaurantByCnpjRepository =
+            new GetRestaurantByCnpjRepositoryStub()
+        const updateRestaurantRepository = new UpdateRestaurantRepositoryStub()
+        const passwordHasherAdapter = new PasswordHasherAdapterStub()
+        const sut = new UpdateRestaurantUseCase(
+            getRestaurantByCnpjRepository,
+            updateRestaurantRepository,
+            passwordHasherAdapter,
+        )
+        return {
+            sut,
+            getRestaurantByCnpjRepository,
+            updateRestaurantRepository,
+            passwordHasherAdapter,
+        }
+    }
+
+    it('should update a restaurant successfully (without cnpj and password)', async () => {
+        const { sut } = makeSut()
+
+        const result = await sut.execute(faker.string.uuid(), {
+            name: faker.company.name(),
+        })
+
+        expect(result).toEqual(restaurant)
+    })
+})
