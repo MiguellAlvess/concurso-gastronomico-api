170b266d46c1023fe4d02eb211d1ba34eadbf145
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-29 09:04:34 -0300

    test: garantindo CreateRestaurantUseCase lançe um erro se IdGeneratorAdapter lançar um erro

--- src/use-cases/restaurant/create-restaurant.test.js
+++ src/use-cases/restaurant/create-restaurant.test.js
@@ -175,4 +175,20 @@ describe('Create Restaurant Use Case', () => {
 
         await expect(promise).rejects.toThrow()
     })
+
+    it('should throw if IdGeneratorAdapter throws', async () => {
+        const { sut, idGeneratorAdapter } = makeSut()
+        import.meta.jest
+            .spyOn(idGeneratorAdapter, 'execute')
+            .mockImplementationOnce(() => {
+                throw new Error()
+            })
+
+        const promise = sut.execute({
+            ...restaurant,
+            imageFilename: 'imagetest.png',
+        })
+
+        await expect(promise).rejects.toThrow()
+    })
 })
