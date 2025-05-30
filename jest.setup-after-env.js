7409a86002d0ab83c9e2f9fb7838c8cdff2475a1
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-30 09:01:17 -0300

    fix: corrigindo ordem de deleção para evitar erro de chave estrangeira

--- jest.setup-after-env.js
+++ jest.setup-after-env.js
@@ -1,8 +1,8 @@
 import { prisma } from './prisma/prisma.js'
 
 beforeEach(async () => {
-    await prisma.user.deleteMany({})
-    await prisma.restaurant.deleteMany({})
-    await prisma.dish.deleteMany({})
     await prisma.review.deleteMany({})
+    await prisma.dish.deleteMany({})
+    await prisma.restaurant.deleteMany({})
+    await prisma.user.deleteMany({})
 })
