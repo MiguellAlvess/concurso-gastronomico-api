1f7a6a4ec0d4408bd1a686734c8d641d1ea09b1c
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-30 09:09:14 -0300

    test: garantindo que o PostgresCreateDishRepository crie o prato no banco de dados

---
+++ src/repositories/dish/create-dish.test.js
@@ -0,0 +1,37 @@
+import { faker } from '@faker-js/faker'
+import { PostgresCreateDishRepository } from './create-dish.js'
+import { prisma } from '../../../prisma/prisma.js'
+
+describe('Create Dish Repository', () => {
+    const createRestaurantParams = {
+        id: faker.string.uuid(),
+        name: faker.company.name(),
+        cnpj: '40.947.067/0001-50',
+        password: faker.internet.password({ length: 6 }),
+        image_url: 'imagetest.png',
+    }
+
+    const createDishParams = {
+        id: faker.string.uuid(),
+        name: faker.commerce.productName(),
+        details: faker.commerce.productDescription().slice(0, 255),
+        price: faker.finance.amount(),
+        restaurant_id: createRestaurantParams.id,
+        image_url: 'imagetest.png',
+    }
+    it('should create dish on db', async () => {
+        await prisma.restaurant.create({
+            data: createRestaurantParams,
+        })
+
+        const sut = new PostgresCreateDishRepository(createDishParams)
+
+        const result = await sut.execute(createDishParams)
+        expect(result.id).toBe(createDishParams.id)
+        expect(result.name).toBe(createDishParams.name)
+        expect(String(result.price)).toBe(String(createDishParams.price))
+        expect(result.details).toBe(createDishParams.details)
+        expect(result.image_url).toBe(createDishParams.image_url)
+        expect(result.restaurant_id).toBe(createDishParams.restaurant_id)
+    })
+})
