3075ecbd83f8a4bd232d8b8c1b1b5792fb1aaef6
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-30 08:43:54 -0300

    test: garantindo que o PostgresCheckUserReviewRepository lançe um erro se o Prisma lançar um erro

--- src/repositories/review/check-user-review.test.js
+++ src/repositories/review/check-user-review.test.js
@@ -16,4 +16,17 @@ describe('Check User Review Repository', () => {
             },
         })
     })
+
+    it('should throw if Prisma throws', async () => {
+        const sut = new PostgresCheckUserReviewRepository()
+        import.meta.jest
+            .spyOn(prisma.review, 'findFirst')
+            .mockImplementationOnce(() => {
+                throw new Error()
+            })
+
+        const promise = sut.execute(review.user_id, dish.id)
+
+        await expect(promise).rejects.toThrow()
+    })
 })
