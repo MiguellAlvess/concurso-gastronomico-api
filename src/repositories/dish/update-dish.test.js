e237235a44ed50be51c817d489d53d959d5d159f
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-31 09:56:50 -0300

    test: garantindo que o PostgresUpdateDishRepository atualize o prato no banco de dados

---
+++ src/repositories/dish/update-dish.test.js
@@ -0,0 +1,50 @@
+import { faker } from '@faker-js/faker'
+import { PostgresUpdateDishRepository } from './update-dish.js'
+import { prisma } from '../../../prisma/prisma.js'
+
+describe('Update Dish Repository', () => {
+    const createRestaurantParams = {
+        id: faker.string.uuid(),
+        name: faker.company.name(),
+        cnpj: '40.947.067/0001-50',
+        password: faker.internet.password({ length: 6 }),
+        image_url: 'imagetest.png',
+    }
+
+    const createDishParams = {
+        id: faker.string.uuid(),
+        name: faker.commerce.productName(),
+        details: faker.commerce.productDescription().slice(0, 255),
+        price: String(faker.finance.amount()),
+        restaurant_id: createRestaurantParams.id,
+        image_url: 'imagetest.png',
+    }
+
+    const updateDishParams = {
+        name: faker.commerce.productName(),
+        details: faker.commerce.productDescription().slice(0, 255),
+        price: String(faker.finance.amount()),
+        restaurant_id: createRestaurantParams.id,
+        image_url: 'imagetest.png',
+    }
+    it('should update a dish on db', async () => {
+        await prisma.restaurant.create({
+            data: createRestaurantParams,
+        })
+        await prisma.dish.create({
+            data: createDishParams,
+        })
+        const sut = new PostgresUpdateDishRepository()
+
+        const result = await sut.execute(createDishParams.id, updateDishParams)
+
+        expect({
+            ...result,
+            price: result.price.toString(),
+        }).toStrictEqual({
+            id: createDishParams.id,
+            ...updateDishParams,
+            price: updateDishParams.price,
+        })
+    })
+})
