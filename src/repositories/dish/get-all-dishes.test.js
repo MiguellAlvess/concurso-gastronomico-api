876f3c191556ee180a00da8ca4e85047e3a66e64
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-31 09:19:54 -0300

    test: garantindo que o PostgresGetAllDishesRepository retorne todos os pratos do banco de dados

---
+++ src/repositories/dish/get-all-dishes.test.js
@@ -0,0 +1,39 @@
+import { faker } from '@faker-js/faker'
+import { PostgresGetAllDishesRepository } from './get-all-dishes.js'
+import { prisma } from '../../../prisma/prisma.js'
+
+describe('Get All Dishes Repository', () => {
+    const createRestaurantParams = {
+        id: faker.string.uuid(),
+        name: faker.company.name(),
+        cnpj: '40.947.067/0001-50',
+        password: faker.internet.password({ length: 6 }),
+        image_url: 'imagetest.png',
+    }
+
+    const createDishParams = {
+        id: faker.string.uuid(),
+        name: faker.commerce.productName(),
+        details: faker.commerce.productDescription().slice(0, 255),
+        price: faker.finance.amount(),
+        restaurant_id: createRestaurantParams.id,
+        image_url: 'imagetest.png',
+    }
+    it('should return all dishes from the db', async () => {
+        await prisma.restaurant.create({
+            data: createRestaurantParams,
+        })
+        await prisma.dish.create({
+            data: createDishParams,
+        })
+        const sut = new PostgresGetAllDishesRepository()
+
+        const result = await sut.execute()
+
+        expect(result[0].id).toBe(createDishParams.id)
+        expect(result[0].name).toBe(createDishParams.name)
+        expect(String(result[0].price)).toBe(String(createDishParams.price))
+        expect(result[0].details).toBe(createDishParams.details)
+        expect(result[0].image_url).toBe(createDishParams.image_url)
+    })
+})
