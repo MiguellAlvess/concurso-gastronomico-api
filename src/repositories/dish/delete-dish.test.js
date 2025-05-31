4bb667e7ad2a7b62ffd84b68cd3161442752feb7
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-05-31 09:10:25 -0300

    test: garantindo que o PostgresDeleteDishRepository delete o prato do banco de dados

---
+++ src/repositories/dish/delete-dish.test.js
@@ -0,0 +1,39 @@
+import { PostgresDeleteDishRepository } from './delete-dish.js'
+import { faker } from '@faker-js/faker'
+import { prisma } from '../../../prisma/prisma.js'
+
+describe('Delete Dish Repository', () => {
+    const createRestaurantParams = {
+        id: faker.string.uuid(),
+        name: faker.company.name(),
+        cnpj: '40.947.067/0001-50',
+        password: faker.internet.password({ length: 6 }),
+        image_url: 'imagetest.png',
+    }
+
+    const createDishParams = {
+        id: faker.string.uuid(),
+        name: faker.commerce.productName(),
+        details: faker.commerce.productDescription().slice(0, 255),
+        price: String(faker.finance.amount()),
+        restaurant_id: createRestaurantParams.id,
+        image_url: 'imagetest.png',
+    }
+
+    it('should delete dish on db', async () => {
+        await prisma.restaurant.create({
+            data: createRestaurantParams,
+        })
+        await prisma.dish.create({
+            data: createDishParams,
+        })
+        const sut = new PostgresDeleteDishRepository(createDishParams.id)
+
+        const result = await sut.execute(createDishParams.id)
+
+        expect({
+            ...result,
+            price: result.price.toString(),
+        }).toStrictEqual(createDishParams)
+    })
+})
