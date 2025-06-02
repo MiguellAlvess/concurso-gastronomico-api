df83214c71925d66f748cc6c6c94ed35fe6dfc27
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-06-02 16:39:13 -0300

    test: garantindo que o PATCH /api/users/me retorne 200 quando o usuário autenticado for atualizado

--- src/routes/users.e2e.test.js
+++ src/routes/users.e2e.test.js
@@ -1,3 +1,4 @@
+import { faker } from '@faker-js/faker'
 import supertest from 'supertest'
 import { user } from '../tests/index.js'
 import { app } from '../app.js'
@@ -24,9 +25,36 @@ describe('User Routes E2E Tests', () => {
 
         const response = await supertest(app)
             .get('/api/users/me')
-            .set('Authorization', `Bearer ${createdUser.tokens.acessToken}`)
+            .set('Authorization', `Bearer ${createdUser.tokens.accessToken}`)
 
         expect(response.statusCode).toBe(200)
         expect(response.body.id).toBe(createdUser.id)
+    })
+
+    it('PATCH /api/users/me should return 200 when user authenticated is updated', async () => {
+        const { body: createdUser } = await supertest(app)
+            .post('/api/users')
+            .send({
+                ...user,
+                id: undefined,
+            })
+
+        const updateUserParams = {
+            first_name: faker.person.firstName(),
+            last_name: faker.person.lastName(),
+            email: faker.internet.email(),
+            password: faker.internet.password(),
+        }
+
+        const response = await supertest(app)
+            .patch('/api/users/me')
+            .set('Authorization', `Bearer ${createdUser.tokens.accessToken}`)
+            .send(updateUserParams)
+
+        expect(response.statusCode).toBe(200)
+        expect(response.body.first_name).toBe(updateUserParams.first_name)
+        expect(response.body.last_name).toBe(updateUserParams.last_name)
+        expect(response.body.email).toBe(updateUserParams.email)
+        expect(response.body.password).not.toBe(updateUserParams.password)
     })
 })
