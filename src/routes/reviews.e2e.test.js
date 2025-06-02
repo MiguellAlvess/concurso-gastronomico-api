fb7adc193a56796ef852714ea272b08cb46366c8
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-06-02 18:53:23 -0300

    test: garantindo que DELETE /api/reviews/me/:reviewId retorne 200 quando a avaliação for deletada

--- src/routes/reviews.e2e.test.js
+++ src/routes/reviews.e2e.test.js
@@ -46,4 +46,53 @@ describe('Dish Routes E2E Tests', () => {
 
         expect(response.statusCode).toBe(201)
     })
+
+    it('DELETE /api/reviews/me/:reviewId should return 200 when review is deleted', async () => {
+        const { body: createdUser } = await supertest(app)
+            .post('/api/users')
+            .send({
+                ...user,
+                id: undefined,
+            })
+
+        const { body: createdRestaurant } = await supertest(app)
+            .post('/api/restaurants')
+            .field('name', restaurant.name)
+            .field('cnpj', restaurant.cnpj)
+            .field('password', restaurant.password)
+            .attach(
+                'image',
+                Buffer.from('fake-image-restaurant'),
+                'imagetest.png',
+            )
+
+        const { body: createdDish } = await supertest(app)
+            .post('/api/dishes/me')
+            .set(
+                'Authorization',
+                `Bearer ${createdRestaurant.tokens.accessToken}`,
+            )
+            .field('name', dish.name)
+            .field('details', dish.details)
+            .field('restaurant_id', createdRestaurant.id)
+            .field('price', dish.price)
+            .attach('image', Buffer.from('fake-image-dish'), 'imagetest.png')
+
+        const { body: createdReview } = await supertest(app)
+            .post(`/api/reviews/me/dishes/${createdDish.id}`)
+            .set('Authorization', `Bearer ${createdUser.tokens.accessToken}`)
+            .send({
+                ...review,
+                id: undefined,
+                user_id: createdUser.id,
+                dish_id: createdDish.id,
+            })
+
+        const response = await supertest(app)
+            .delete(`/api/reviews/me/${createdReview.id}`)
+            .set('Authorization', `Bearer ${createdUser.tokens.accessToken}`)
+
+        expect(response.statusCode).toBe(200)
+        expect(response.body.id).toBe(createdReview.id)
+    })
 })
