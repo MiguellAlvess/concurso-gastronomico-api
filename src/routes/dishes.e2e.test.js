c1fdb24d54459f646851beb25ef6539acc5b6d75
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-06-02 18:32:43 -0300

    test: garantindo que GET /api/dishes/me  retorne 200 quando pegar todos os pratos do restaurante

--- src/routes/dishes.e2e.test.js
+++ src/routes/dishes.e2e.test.js
@@ -64,4 +64,39 @@ describe('Dish Routes E2E Tests', () => {
         expect(response.statusCode).toBe(200)
         expect(response.body.id).toBe(createdDish.id)
     })
+
+    it('GET /api/dishes/me should return 200 when fetching dishes successfully', async () => {
+        const { body: createdRestaurant } = await supertest(app)
+            .post('/api/restaurants')
+            .field('name', restaurant.name)
+            .field('cnpj', restaurant.cnpj)
+            .field('password', restaurant.password)
+            .attach(
+                'image',
+                Buffer.from('fake-image-restaurant'),
+                'imagetest.png',
+            )
+
+        const { body: createdDish } = await supertest(app)
+            .post('/api/dishes/me')
+            .set(
+                'Authorization',
+                `Bearer ${createdRestaurant.tokens.accessToken}`,
+            )
+            .field('name', dish.name)
+            .field('details', dish.details)
+            .field('restaurant_id', createdRestaurant.id)
+            .field('price', dish.price)
+            .attach('image', Buffer.from('fake-image-dish'), 'imagetest.png')
+
+        const response = await supertest(app)
+            .get(`/api/dishes/?restaurant_id=${createdRestaurant.id}`)
+            .set(
+                'Authorization',
+                `Bearer ${createdRestaurant.tokens.accessToken}`,
+            )
+
+        expect(response.statusCode).toBe(200)
+        expect(response.body[0].id).toBe(createdDish.id)
+    })
 })
