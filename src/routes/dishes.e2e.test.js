e274b55117e29a9c25fe3ff0a3498e3b21d0a42e
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-06-02 18:36:01 -0300

    test: garantindo que PATCH /api/dishes/me/:dishId retorne 200 quando o prato for atualizado

--- src/routes/dishes.e2e.test.js
+++ src/routes/dishes.e2e.test.js
@@ -1,6 +1,7 @@
 import supertest from 'supertest'
 import { app } from '../app.js'
 import { restaurant, dish } from '../tests/index.js'
+import { faker } from '@faker-js/faker'
 
 describe('Dish Routes E2E Tests', () => {
     it('POST /api/dishes/me should return 201 when dish is created', async () => {
@@ -98,5 +99,45 @@ describe('Dish Routes E2E Tests', () => {
 
         expect(response.statusCode).toBe(200)
         expect(response.body[0].id).toBe(createdDish.id)
+    })
+
+    it('PATCH /api/dishes/me/:dishId should return 200 when dish is updated', async () => {
+        const { body: createdRestaurant } = await supertest(app)
+            .post('/api/restaurants')
+            .field('name', restaurant.name)
+            .field('cnpj', restaurant.cnpj)
+            .field('password', restaurant.password)
+            .attach(
+                'image',
+                Buffer.from('fake-image-restaurant'),
+                'imagetest.png',
+            )
+
+        const { body: createdDish } = await supertest(app)
+            .post('/api/dishes/me')
+            .set(
+                'Authorization',
+                `Bearer ${createdRestaurant.tokens.accessToken}`,
+            )
+            .field('name', dish.name)
+            .field('details', dish.details)
+            .field('restaurant_id', createdRestaurant.id)
+            .field('price', dish.price)
+            .attach('image', Buffer.from('fake-image-dish'), 'imagetest.png')
+
+        const updateDishParams = {
+            name: faker.lorem.word(),
+        }
+
+        const response = await supertest(app)
+            .patch(`/api/dishes/me/${createdDish.id}`)
+            .set(
+                'Authorization',
+                `Bearer ${createdRestaurant.tokens.accessToken}`,
+            )
+            .send(updateDishParams)
+
+        expect(response.statusCode).toBe(200)
+        expect(response.body.name).toBe(updateDishParams.name)
     })
 })
