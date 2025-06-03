306e36a2c27c5eb4fdb6c0f836244e96ddb1d752
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-06-03 09:52:17 -0300

    test: garantindo que o GetDishReviewsUseCas pegue as avaliações do prato corretamente

---
+++ src/use-cases/dish/get-dish-reviews.test.js
@@ -0,0 +1,34 @@
+import { GetDishReviewsUseCase } from './get-dish-reviews'
+import { dish } from '../../tests/index.js'
+
+describe('Get Dish Reviews Use Case', () => {
+    class GetDishReviewsRepository {
+        async execute() {
+            return dish
+        }
+    }
+    class GetDishByIdRepository {
+        async execute() {
+            return dish
+        }
+    }
+
+    const makeSut = () => {
+        const getDishReviewsRepository = new GetDishReviewsRepository()
+        const getDishByIdRepository = new GetDishByIdRepository()
+        const sut = new GetDishReviewsUseCase(
+            getDishReviewsRepository,
+            getDishByIdRepository,
+        )
+
+        return { sut, getDishReviewsRepository, getDishByIdRepository }
+    }
+
+    it('should get dish reviews successfully', async () => {
+        const { sut } = makeSut()
+
+        const getDishReviews = await sut.execute(dish.id)
+
+        expect(getDishReviews).toBeTruthy()
+    })
+})
