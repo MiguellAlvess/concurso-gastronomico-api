6761834e99d7be8e73f095cbaf3732f3d8e9b96e
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-06-03 17:17:43 -0300

    test: garantindo que o UpdateDishUseCase atualize o prato corretamente

---
+++ src/use-cases/dish/update-dish.test.js
@@ -0,0 +1,40 @@
+import { UpdateDishUseCase } from './update-dish.js'
+import { dish } from '../../tests/index.js'
+
+describe('Update Dish Use Case', () => {
+    class UpdateDishRepositoryStub {
+        async execute() {
+            return dish
+        }
+    }
+
+    class GetDishByIdRepositoryStub {
+        async execute() {
+            return dish
+        }
+    }
+
+    const makeSut = () => {
+        const updateDishRepository = new UpdateDishRepositoryStub()
+        const getDishByIdRepository = new GetDishByIdRepositoryStub()
+        const sut = new UpdateDishUseCase(
+            updateDishRepository,
+            getDishByIdRepository,
+        )
+        return { sut, updateDishRepository, getDishByIdRepository }
+    }
+
+    it('should update dish successfully', async () => {
+        const { sut } = makeSut()
+
+        const updatedDish = await sut.execute(dish.id, {
+            ...dish,
+            imageFilename: 'imagetest.png',
+        })
+
+        expect(updatedDish).toEqual({
+            ...dish,
+            image_url: 'imagetest.png',
+        })
+    })
+})
