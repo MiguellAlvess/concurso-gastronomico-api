e3f8f0bf67f6f8ad564ef6d051d13be3c7e5c5e4
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-06-08 12:31:51 -0300

    refactor: padrozinando importaçõe do DeleteUserController

--- src/controllers/user/delete-user.js
+++ src/controllers/user/delete-user.js
@@ -5,7 +5,7 @@ import {
     ok,
     userNotFoundResponse,
 } from '../helpers/index.js'
-import { UserNotFoundError } from '../../errors/user.js'
+import { UserNotFoundError } from '../../errors/index.js'
 
 export class DeleteUserController {
     constructor(deleteUserUseCase) {
